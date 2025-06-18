e289826939d029e05c3d9ca36c43b515f45e51e6
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-18 21:20:51 +0530

    added

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello ")
+const wrapper = document.querySelector(".wrapper");
+const backBtn = document.querySelector(".back-btn");
+const menuBtn = document.querySelector(".menu-btn");
+
+const toggleScreen = () => {
+    wrapper.classList.toggle("show-category");
+};
+
+menuBtn.addEventListener("click", toggleScreen);
+backBtn.addEventListener("click", toggleScreen);
